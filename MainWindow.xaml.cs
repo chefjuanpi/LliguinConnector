f0b6f92b4556c4b5438bfa5c840e9364515435f7
el Pablo <pablo@lliguin.com>
2018-10-27 19:48:44 -0400

    apply material design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,46 @@ using System.Windows.Shapes;
 
 namespace LliguinQBLinker
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
     }
+
+    private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+    {
+      ButtonCloseMenu.Visibility = Visibility.Visible;
+      ButtonOpenMenu.Visibility = Visibility.Collapsed;
+    }
+
+    private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+    {
+      ButtonCloseMenu.Visibility = Visibility.Collapsed;
+      ButtonOpenMenu.Visibility = Visibility.Visible;
+    }
+
+    private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      UserControl usc = null;
+      GridMain.Children.Clear();
+
+      switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+      {
+        case "ItemHome":
+          GridMain.Children.Add(new UserControlHome());
+          break;
+
+        case "ItemCreate":
+          GridMain.Children.Add(new UserControlCreate());
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
 }
